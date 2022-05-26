832011f877f504559156417a598174366ea89fbc
MrWider <104024820+MrWider@users.noreply.github.com>
2022-05-26 15:08:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -240,7 +240,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user','*')
 
         #Descomentar debajo solo si se ba a poner el usuario admin de telegram manual
-        #tl_admin_user = '*'
+        #tl_admin_user = 'Mr_Wider'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -559,7 +559,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #decomentar abajo y modificar solo si se va a poner el token del bot manual
-    #bot_token = 'BOT TOKEN'
+    #bot_token = '5305623017:AAFR335M_NarHorh_IM0UTlYRB92JJufMM4'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
